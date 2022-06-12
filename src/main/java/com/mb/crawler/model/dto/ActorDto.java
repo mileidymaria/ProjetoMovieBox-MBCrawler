f7c84549bd3729ac0e878c980c11beeb0ba3037c
mileidy <mileidymaria01@gmail.com>
2022-06-12 20:19:27 -0300

    Documentando a aplicação com SWAGGER utilizando Springdoc

--- src/main/java/com/mb/crawler/model/dto/ActorDto.java
+++ src/main/java/com/mb/crawler/model/dto/ActorDto.java
@@ -9,4 +9,20 @@ public class ActorDto {
         this.nome = nome;
         this.hashNome = hashNome;
     }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getHashNome() {
+        return hashNome;
+    }
+
+    public void setHashNome(int hashNome) {
+        this.hashNome = hashNome;
+    }
 }
